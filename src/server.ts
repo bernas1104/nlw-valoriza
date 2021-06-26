bf43b6635e7bd99c8e781626c414fbe375d0ffe5
Bernardo Costa Nascimento <bernardoc1104@gmail.com>
2021-06-26 18:44:14 -0300

    chore(server): ajustes no setup do servidor

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,8 @@
 import 'reflect-metadata';
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
-import './database';
-import router from './routes';
+import './domain/database';
+import router from './api/routes';
 
 const app = express();
 
